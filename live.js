db40ce659d54008f37b08ced5b11e4b662b25f8c
Llamahere <aa99919@gmail.com>
2023-12-15 13:58:19 +0800

    Update live.js

--- live.js
+++ live.js
@@ -16,7 +16,36 @@ class Life{
             
         }
     }
+
+    // calcNeighbor = function(row,col){
+
+    // }
+}
+Life.prototype.statusAt = function(row, col){
+    if(row<0 || col<0 || row>=this.row || col>=this.col)
+      return DEAD;
+    else
+      return this.grid[row][col];
+}
+Life.prototype.calcNeighbor = function(row, col){
+    var count=0;
+    count += this.statusAt(row-1, col-1)//lfet top
+    count += this.statusAt(row-1, col+0)//up
+    count += this.statusAt(row-1, col+1)//right top
+    count += this.statusAt(row+0, col-1)//left
+    count += this.statusAt(row+0, col+1)//right
+    count += this.statusAt(row+1, col-1)//lfet bottom
+    count += this.statusAt(row+1, col+0)//down
+    count += this.statusAt(row+1, col+1)//right bottom
+    return count;
 }
 
+
+
 var game1 = new Life(3,3);
-var game2 = new Life(30,30);
+game1.grid[1][0]=LIVE;
+game1.grid[1][1]=LIVE;
+game1.grid[1][2]=LIVE;
+console.log("(1,1):"+game1.calcNeighbor(1,1));
+console.log("(2,0):"+game1.calcNeighbor(2,0));
+//var game2 = new Life(30,30);
